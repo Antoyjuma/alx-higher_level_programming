b805e26fc3ad0af859a2ff43731f5f3d78d33f2d
Antonyjuma <antoymalobajuma@gmail.com>
2024-03-12 05:48:28 -0700

    lists.h

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list..
+ * @n: integer
+ * @next: points to the next node.
+ *
+ * Description: singly linked list node struc..
+ * 
+ */
+typedef struct listint_s
+{
+    int j;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int j);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
