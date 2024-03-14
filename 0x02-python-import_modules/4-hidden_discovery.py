a71fa2c555e63b81534867c287d006627971280b
Antonyjuma <antoymalobajuma@gmail.com>
2024-03-14 07:18:49 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import hidden_4
-    d = dir
-    for i in range(0, len(d)):
-        if d[i][0:2] != "__":
-            print("{}".format(d[i]))
+    all_dir = dir(hidden_4)
+    avoid = "__"
+    for i in range(0, len(all_dir)):
+        if avoid not in all_dir[i]:
+            print(all_dir[i])
