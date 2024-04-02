570f6659dc4284bc66a595bf7a6e6a5622e2bcc0
Antonyjuma <antoymalobajuma@gmail.com>
2024-04-02 14:21:07 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,15 +6,25 @@
 class Square:
     """
     square class with a private attribute size
-    and checks data type
     """
     def __init__(self, size=0):
-        if type(size) is not int:
+        self.size = size
+
+    @property
+    def size(self):
+        """retrieve the attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """modify the attribute value"""
+
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """returns the current square area"""
